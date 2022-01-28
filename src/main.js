01c61dd7dcb028a8019e854347ff897ab7b95738
Igor Decess <i.decess@comcast.net>
2022-01-27 21:38:13 -0700

    Add correct event listeners for make cover and saved cover pages

--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ var viewSavedCoversBtn = document.querySelector('.view-saved-button');
 var makeNewCoverBtn = document.querySelector('.make-new-button');
 var homeBtn = document.querySelector('.home-button');
 
+var homePage = document.querySelector('.home-view');
+var makeYourOwnPage = document.querySelector('.form-view');
+var savedCoversPage = document.querySelector('.saved-view');
+
+
+
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -19,9 +26,12 @@ var currentCover;
 // Add your event listeners here 👇
 window.onload = changeCover();
 randomCoverBtn.addEventListener('click', changeCover);
-makeNewCoverBtn.addEventListener('click', makeNewCover);
+makeNewCoverBtn.addEventListener('click', makeNewCoverPage);
 
 // Create your event handlers and other functions here 👇
+
+
+
 function changeCover() {
   currentCover = new Cover(coverImage.src, coverTitle.innerText, taglineOne.innerText, taglineTwo.innerText);
   coverImage.src = covers[getRandomIndex(covers)];
@@ -29,6 +39,8 @@ function changeCover() {
   taglineOne.innerText = descriptors[getRandomIndex(descriptors)];
   taglineTwo.innerText = descriptors[getRandomIndex(descriptors)];
 }
+
+
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
